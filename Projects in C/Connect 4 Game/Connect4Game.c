ad73bbea9e2939aa3753f5af2dd17daa80373f33
Francisco Saavedra <saavedra.francisco@outlook.com>
2020-09-07 19:23:33 -0500

    Adding setup for functions

---
+++ Projects in C/Connect 4 Game/Connect4Game.c
@@ -0,0 +1,70 @@
+/*
+    Francisco Saavedra 
+    Connect 4 Game
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+const int MAX_ROWS = 7;
+const int MAX_COLS = 10;
+const int BLANK = 0;
+
+int main() 
+{
+
+}
+
+void initGrid(int theGrid[][MAX_COLS], int rows, int cols)
+{
+    
+}
+
+void printGrid(const int theGrid[][MAX_COLS], int rows, int cols)
+{
+
+}
+
+char takeMove(int theGrid[][MAX_COLS], int rows, int cols, int targetColumn, int whichPlayer)
+{
+    
+}
+
+void askUserForMove(int theGrid[][MAX_COLS], int rows, int cols, int whichPlayer)
+{
+
+}
+
+void eatInputLine()
+{
+
+}
+
+/*
+    return 0, 1 or 2
+    0 no winner
+*/
+int checkForWinner(int theGrid[][MAX_COLS], int rows, int cols)
+{
+
+}
+
+int checkForVerticalWinner(int theGrid[][MAX_COLS], int rows, int cols)
+{
+
+}
+
+int checkForHorizontalWinner(int theGrid[][MAX_COLS], int rows, int cols)
+{
+
+}
+
+int checkForRightDiagonalWinner(int theGrid[][MAX_COLS], int rows, int cols)
+{
+
+}
+
+int checkForLeftDiagonalWinner(int theGrid[][MAX_COLS], int rows, int cols)
+{
+
+}
